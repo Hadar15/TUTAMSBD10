5d7d0153091d2380a3de664a2a67dde7645316df
Hadar15 <haidar.r2005@gmail.com>
2026-05-02 11:15:38 +0700

    dix inset

--- api/db.js
+++ api/db.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const connectionString = 
+let connectionString = 
   process.env.POSTGRES_URL_NON_POOLING || 
   process.env.POSTGRES_PRISMA_URL || 
   process.env.POSTGRES_URL || 
   process.env.DATABASE_URL;
+
+
+if (connectionString && connectionString.includes('?')) {
+  connectionString = connectionString.split('?')[0];
+}
 
 const pool = new Pool({
   connectionString,
